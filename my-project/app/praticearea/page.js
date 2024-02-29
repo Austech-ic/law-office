70851dfe1952392b2f9983e508242b3d4ae4eee2
DevMayani <106169881+DevMayani@users.noreply.github.com>
2024-02-29 23:09:48 +0100

    add new

--- my-project/app/praticearea/page.js
+++ my-project/app/praticearea/page.js
@@ -1,8 +1,23 @@
+import Header from '@/components/website/header/header'
+import Navbar from '@/components/website/navbar/navbar'
 import React from 'react'
+import styles from './praticearea.module.css'
+import Pratsectionone from '@/components/pratarea/pratsectionone'
+import Footer from '@/components/website/footer/footer'
+import Pratsectiontwo from '@/components/pratarea/pratsectiontwo'
 
 const page = () => {
   return (
-    <div>page</div>
+   <div>
+     <div className={styles.main}>
+      <Header />
+      <Navbar />
+      <Pratsectionone />
+    </div>
+    <Pratsectiontwo />
+    <Footer />
+   
+   </div>
   )
 }
 
